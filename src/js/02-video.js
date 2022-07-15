830bdd59ebb1da2d134718c9a1fa4ebb5197de1d
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2022-07-15 17:55:18 +0300

    task-02-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player'; // Импортируем vimeo
+import throttle from 'lodash.throttle'; // Импортируем throttle
+
+// Инициализируем плеер
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+// Отслеживаем время и записываем в локальное хранилище
+const onPlay = function(data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000)); // Отслеживаем время один раз в секунду
+
+const savedCurrentTime = localStorage.getItem(STORAGE_KEY);
+
+if (savedCurrentTime) {
+    player.setCurrentTime(savedCurrentTime); // Возобновляем просмотр после перезагрузки
+}
+
+
+
 
