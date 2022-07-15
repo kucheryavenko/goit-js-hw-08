ff08e9ac1901f5e60ab3d2b617f8b277a6273883
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2022-07-15 11:32:37 +0300

    task-01-done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox"; // Описан в документации
+import "simplelightbox/dist/simple-lightbox.min.css"; // Дополнительный импорт стилей
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup); // Рендерим элементы разметки  
+
+// Функция createImageCardsMarkup создаёт разметку по массиву данных galleryItems
+function createImageCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+     <li>
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`;
+    })
+    .join('');
+}; 
+
+// Запускаем библиотеку SimpleLightbox и выводим 'alt' c задержкой 250 ms
+var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt'});
