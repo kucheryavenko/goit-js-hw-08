f67ae5cf77082959850a8461dea957ce4bf63b8d
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2022-07-17 21:03:37 +0300

    bugfix-task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,7 @@ import throttle from 'lodash.throttle'; // Импортируем throttle
 
 const formEl = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
+let formData = {};
 
 formEl.addEventListener('submit', onFormSubmit);
 formEl.addEventListener('input', throttle(onFormInput, 500));
@@ -24,6 +24,13 @@ function onFormSubmit(evt) {
 
 // Отслеживаем инпуты на форме и добавляем их значения в хранилище
 function onFormInput(evt) {
+
+    if (localStorage.getItem(STORAGE_KEY)) {
+        formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    } else {
+        formData = {};
+    }
+
     formData[evt.target.name] = evt.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
@@ -38,4 +45,46 @@ function populateFormInput() {
         formEl.message.value = parseValueInput.message || '';
     }
 }
+
+// Вариант с багом
+// import throttle from 'lodash.throttle'; // Импортируем throttle
+
+// const formEl = document.querySelector('.feedback-form');
+// const STORAGE_KEY = 'feedback-form-state';
+// const formData = {};
+
+// formEl.addEventListener('submit', onFormSubmit);
+// formEl.addEventListener('input', throttle(onFormInput, 500));
+
+// populateFormInput();
+
+// // Сабмитим форму
+// function onFormSubmit(evt) {
+//     evt.preventDefault();
+
+//     if (formEl.email.value === '') {
+//         return alert('Email field must be filled!');
+//     }
+
+//     evt.currentTarget.reset();
+//     localStorage.removeItem(STORAGE_KEY);
+//     console.log(formData);  
+// }
+
+// // Отслеживаем инпуты на форме и добавляем их значения в хранилище
+// function onFormInput(evt) {
+//     formData[evt.target.name] = evt.target.value;
+//     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// }
+
+// // Возвращаем значения из хранилища в форму
+// function populateFormInput() {
+//     const savedValueInput = localStorage.getItem(STORAGE_KEY);
+//     const parseValueInput = JSON.parse(savedValueInput);
+    
+//     if (savedValueInput) {
+//         formEl.email.value = parseValueInput.email || '';
+//         formEl.message.value = parseValueInput.message || '';
+//     }
+// }
 
